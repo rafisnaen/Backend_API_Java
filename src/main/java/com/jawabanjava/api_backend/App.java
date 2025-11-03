01538a362c08f56faeb1b762752f30a2daff27af
Muhammad Rafi Isnaen <muhammadrafiisnaen@gmail.com>
2025-11-03 22:17:21 +0700

    Initial maven project with Spring Boot and MySQL Dependency

---
+++ src/main/java/com/jawabanjava/api_backend/App.java
@@ -0,0 +1,10 @@
+package com.jawabanjava.api_backend;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
